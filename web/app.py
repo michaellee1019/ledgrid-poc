77438ba2ebf2bbae2ea825a62ba6e63af6902ee2
Ryan Timmons <ryan@rytim.com>
2026-09-12 02:38:32 -0400

    Refresh Composer cache for Gallery detail

--- web/app.py
+++ web/app.py
@@ -156,7 +156,7 @@ from web.working_draft_store import WorkingDraftStore, WorkingDraftError
 from web.composer_final_preview import ComposerFinalPreview, current_component_catalog
 
 
-COMPOSER_SHELL_VERSION = "composer-shell-v15"
+COMPOSER_SHELL_VERSION = "composer-shell-v16"
 CANONICAL_BROWSER_SCENE_SCHEMA = "ledgrid.browser-scene-v2"
 
 # The Gallery stays projected from the Scene v2 packet, while this small map
